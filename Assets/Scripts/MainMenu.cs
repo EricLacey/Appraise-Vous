02c673661a000c59e1311c6991b76e7693e31bd3
Eric Lacey <34018178+EricLacey@users.noreply.github.com>
2020-12-03 12:33:05 -0500

    Ui update (#9)
    * pull from main to UI (#7)
    
    * C commit Wed. Night
    
    Beta assets, changed volume for sounds, tried to get manual tearing fixed but didn't make much headway.
    
    * Trouble with pedestal mesh, reverted
    
    Also made sounds even quieter
    
    * enlarged pageturn hotspots
    
    * Magnifying glass (#6)
    
    * Lens Made
    
    * Magnifying glass functional
    
    Co-authored-by: ConorMulcahy118 <cemulcahy@gmail.com>
    Co-authored-by: Conor Mulcahy <20425948+ConorMulcahy118@users.noreply.github.com>
    
    * Main Menu Done
    
    * added menu music
    
    Co-authored-by: ConorMulcahy118 <cemulcahy@gmail.com>
    Co-authored-by: Conor Mulcahy <20425948+ConorMulcahy118@users.noreply.github.com>

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public enum menuStates { Title, Context, Character};
+    public menuStates currentState;
+
+    public GameObject titlePanel;
+    public GameObject contextPanel;
+    public GameObject CharacterMenu;
+
+    private void Awake()
+    {
+        currentState = menuStates.Title;
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case menuStates.Title:
+                titlePanel.SetActive(true);
+                contextPanel.SetActive(false);
+                CharacterMenu.SetActive(false);
+                break;
+            case menuStates.Context:
+                titlePanel.SetActive(false);
+                contextPanel.SetActive(true);
+                CharacterMenu.SetActive(false);
+                break;
+            case menuStates.Character:
+                titlePanel.SetActive(false);
+                contextPanel.SetActive(false);
+                CharacterMenu.SetActive(true);
+                break;
+            default:
+                break;
+
+        }
+    }
+
     public void GoToBook()
     {
         SceneManager.LoadScene("BookScene");
@@ -18,5 +55,13 @@ public class MainMenu : MonoBehaviour
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+    public void OnTitleBtn()
+    {
+        currentState = menuStates.Context;
+    }
+    public void OnContextBtn()
+    {
+        currentState = menuStates.Character;
     }
 }
