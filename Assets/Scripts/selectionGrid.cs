18bc489d6be8093ab6333442ece6e65ae6eb72b4
Eric Lacey <34018178+EricLacey@users.noreply.github.com>
2020-12-10 10:53:33 -0500

    merge main into updated screens

--- Assets/Scripts/selectionGrid.cs
+++ Assets/Scripts/selectionGrid.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class selectionGrid : MonoBehaviour
 {
-    int selGridInt = 0;
-    string[] selStrings = { "radio1", "radio2", "radio3" };
+    int timeGridInt = 1;
+    int numGridInt = 1;
+    public List<Sprite> timeSprites;
+    public List<Sprite> numSprites;
+    Texture[] timeTextures = new Texture[3];
+    Texture[] numTextures = new Texture[3];
+
+    void OnEnable()
+    {
+
+        for (int i = 0; i < timeSprites.Count; i++)
+        {
+            timeTextures[i] = textureFromSprite(timeSprites[i]);
+        }
+
+        for (int i = 0; i < numSprites.Count; i++)
+        {
+            numTextures[i] = textureFromSprite(numSprites[i]);
+        }
+    }
+
 
     void OnGUI()
     {
-        GUILayout.BeginHorizontal("Box");
-        selGridInt = GUILayout.SelectionGrid(selGridInt, selStrings, 3);
-        if (GUILayout.Button("Start"))
+        timeGridInt = GUILayout.SelectionGrid(timeGridInt, timeTextures, 3);
+        numGridInt = GUILayout.SelectionGrid(numGridInt, numTextures, 3);
+    }
+
+
+    //from https://answers.unity.com/questions/651984/convert-sprite-image-to-texture.html
+    public static Texture2D textureFromSprite(Sprite sprite)
+    {
+        if (sprite.rect.width != sprite.texture.width)
         {
-            Debug.Log("You chose " + selStrings[selGridInt]);
+            Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+            Color[] newColors = sprite.texture.GetPixels((int)sprite.textureRect.x,
+                                                         (int)sprite.textureRect.y,
+                                                         (int)sprite.textureRect.width,
+                                                         (int)sprite.textureRect.height);
+            newText.SetPixels(newColors);
+            newText.Apply();
+            return newText;
         }
-        GUILayout.EndVertical();
+        else
+            return sprite.texture;
     }
 }
 
